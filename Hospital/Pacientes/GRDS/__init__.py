7e42da79c996cab55cb5145d5af903a060a0adc6
Sebastian Rivera <svrivera@uc.cl>
2017-09-26 22:42:54 -0300

    Cambios en la manera de leer GRDs

--- Hospital/Pacientes/GRDS/__init__.py
+++ Hospital/Pacientes/GRDS/__init__.py
@@ -3,17 +3,18 @@ from .GRD import *
 
 # Abrir archivo e instanciar las cosas con los valores respectivos
 
-# GRD_Coronario = Coronario(1,4,6,7,88,8)
-GRD_Coronario = Coronario()
-GRD_Hepatico = Hepatico()
-GRD_Respiratorio = Respiratorio()
-GRD_Renal = Renal()
-GRD_Neurologico = Neurologico()
-GRD_Traumatologico = Traumatologico()
-GRD_Esofagico = Esofagico()
-GRD_Oftalmologico = Oftalmologico()
-GRD_Circulatorio = Circulatorio()
-GRD_Intestinal = Intestinal()
+
+GRD_Coronario = Coronario(nombre="Coronario", tiempo_recomendado=(3, 4, 4), tiempo_minimo=(1, 2, 4), ponderador_c=3,
+                          ponderador_i=3, costo_externalizacion=7.53)
+GRD_Hepatico = Hepatico("Hepatico", (1, 2, 2), (1, 1, 2), 2, 2, 3.34)
+GRD_Respiratorio = Respiratorio("Respiratorio", (1, 2, 2), (1, 1, 2), 2, 2, 3.39)
+GRD_Renal = Renal("Renal", (2, 3, 3), (1, 1, 3), 3, 3, 3.30)
+GRD_Neurologico = Neurologico("Neurologico", (2, 2, 2), (1, 1, 2), 3, 3, 3.3)
+GRD_Traumatologico = Traumatologico("Traumatologico", (2, 3, 2), (1, 2, 2), 2, 2, 4.96)
+GRD_Esofagico = Esofagico("Esofagico", (0, 2, 2), (0, 1, 2), 0, 2, 2.81)
+GRD_Oftalmologico = Oftalmologico("Oftalmologico", (0, 2, 1), (0, 1, 1), 0, 1, 2.18)
+GRD_Circulatorio = Circulatorio("Circulatorio", (2, 2, 1), (1, 1, 1), 2, 2, 2.82)
+GRD_Intestinal = Intestinal("Intestinal", (0, 4, 4), (0, 2, 4), 0, 2, 5.25)
 
 # Guardamos las funciones en un nombre más entendible
 
